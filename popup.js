77109749e571f3ab7befc8cf1f8f2c60da362842
Hunter Gerlach <hunter@huntergerlach.com>
2022-12-04 12:38:30 -0500

    Refreshed the list of saved strings after adding a new string.

--- popup.js
+++ popup.js
@@ -1,6 +1,45 @@
 // Get the container element for the saved strings
 var container = document.getElementById('saved-strings-container');
 
+// Define a function to update the list of saved strings
+function updateSavedStrings() {
+    // Clear the container element
+    container.innerHTML = '';
+  
+    // Get the list of saved strings from storage
+    chrome.storage.sync.get(['savedStrings'], function(result) {
+      // Check if the list of saved strings exists
+      if (result.savedStrings) {
+        // Loop through the list of saved strings
+        for (var i = 0; i < result.savedStrings.length; i++) {
+          // Create a new element for the saved string
+          var stringElement = document.createElement('div');
+  
+          // Set the text of the element to the saved string
+          stringElement.textContent = result.savedStrings[i];
+  
+          // Add an event listener for the click event on the string element
+          stringElement.addEventListener('click', function() {
+            // Get the selected string from the text of the element
+            var selectedString = this.textContent;
+  
+            // Check if the chrome and chrome.tabs objects are defined
+            if (typeof chrome !== 'undefined' && typeof chrome.tabs !== 'undefined') {
+              // Use the chrome.tabs.query function to find the active tab
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                // Send a message to the content script of the active tab with the selected string
+                chrome.tabs.sendMessage(tabs[0].id, {selectedString: selectedString});
+              });
+            }
+          });
+  
+          // Add the element to the container
+          container.appendChild(stringElement);
+        }
+      }
+    });
+  }
+
 // Get the list of saved strings from storage
 chrome.storage.sync.get(['savedStrings'], function(result) {
   // Check if the list of saved strings exists
@@ -40,38 +79,58 @@ var input = document.getElementById('string-input');
 // Get the save button
 var saveButton = document.getElementById('save-button');
 
-// Define a function to save a string to storage
 function saveString() {
-  // Get the list of saved strings from storage
-  chrome.storage.sync.get(['savedStrings'], function(result) {
-    // Check if the list of saved strings exists
-    if (result.savedStrings) {
-      // Add the new string to the list of saved strings
-      result.savedStrings.push(input.value);
+    // Get the list of saved strings from storage
+    chrome.storage.sync.get(['savedStrings'], function(result) {
+      // Check if the list of saved strings exists
+      if (result.savedStrings) {
+        // Add the new string to the list of saved strings
+        result.savedStrings.push(input.value);
+  
+        // Save the updated list of saved strings to storage
+        chrome.storage.sync.set({savedStrings: result.savedStrings}, function() {
+          // After saving the updated list, refresh the list of saved strings
+          updateSavedStrings();
+        });
+      } else {
+        // Create a new list of saved strings with the new string
+        var savedStrings = [input.value];
+  
+        // Save the new list of saved strings to storage
+        chrome.storage.sync.set({savedStrings: savedStrings}, function() {
+          // After saving the new list, refresh the list of saved strings
+          updateSavedStrings();
+        });
+      }
+    });
+  }
+  
 
-      // Save the updated list of saved strings to storage
-      chrome.storage.sync.set({savedStrings: result.savedStrings});
-    } else {
-      // Create a new list of saved strings with the new string
-      var savedStrings = [input.value];
-
-      // Save the new list of saved strings to storage
-      chrome.storage.sync.set({savedStrings: savedStrings});
-    }
-  });
-}
+  // Update the list of saved strings
+updateSavedStrings();
 
 // Add an event listener for the click event on the save button
-saveButton.addEventListener('click', saveString);
+saveButton.addEventListener('click', function() {
+    // Save the string in the input element
+    saveString();
+    
+    // Update the list of saved strings
+    updateSavedStrings();
+  });
+  
 
 // Define a function to handle the keydown event on the input element
 function handleKeydown(event) {
     // Check if the "Enter" key was pressed
     if (event.keyCode === 13) {
-      // Call the saveString() function
+      // Save the string in the input element
       saveString();
+  
+      // Update the list of saved strings
+      updateSavedStrings();
     }
-  }
+}
+
   
   // Add an event listener for the keydown event on the input element
   input.addEventListener('keydown', handleKeydown);
